b3b356433a4a0b5a538db0f57affc6111c354079
ccQpein <ccqpein@protonmail.com>
2018-12-05 17:37:46 -0500

    use new algorithem

--- 2018/day5.lisp
+++ 2018/day5.lisp
@@ -18,16 +18,35 @@
 			(incf ind))
 		))))
 
+(defun stack-handle (char-list)
+  "second version of day5, this method make thing go in O(n)"
+  (let ((len (length char-list)))
+	(do ((this-char 1 (incf this-char))
+		 (stack (list (car char-list))))
+		((eql this-char len) stack)
+	  (cond ((eql nil stack)
+			 (setf stack (cons (nth this-char char-list) stack)))
+			((case-match (nth this-char char-list)
+						 (car stack))
+			 (setf stack (cdr stack)))
+			(t
+			 (setf stack (cons (nth this-char char-list) stack)))))))
+
+(defun day5-v2 (filepath)
+  "use new stack-handle"
+  (let ((cache (read-all-input-together filepath)))
+	(length (stack-handle cache))))
+
 (defun day5-part2 (filepath)
   (let ((cache (read-all-input-together filepath))
 		(alphabet (mapcar #'list
 						  (concatenate 'list "abcdefghigklmnopqrstuvwxyz")
 						  (concatenate 'list "ABCDEFGHIJKLMNOPQRSTUVWXYZ"))))
 	(loop
-	  for i from 0 to 25
-	  for ab = (nth i alphabet)
-	  collect (let ((inner-cache (remove-from-list cache ab))
-					)
+	   for i from 0 to 25
+	   for ab = (nth i alphabet)
+	   collect (let ((inner-cache (remove-from-list cache ab))
+					 )
 				(do* ((need-to-delete '(1))
 					  (this-line inner-cache (clean-list this-line need-to-delete))
 					  )
@@ -38,10 +57,21 @@
 				  (do ((ind 0))
 					  ((>= ind (- (length this-line) 1)))
 					(if (case-match (nth ind this-line) (nth (1+ ind) this-line))
-				   (setf need-to-delete (append need-to-delete (list ind (1+ ind)))
-						 ind (+ 2 ind))
-				   (incf ind))
-			   ))))))
+						(setf need-to-delete (append need-to-delete (list ind (1+ ind)))
+							  ind (+ 2 ind))
+						(incf ind))
+					))))))
+
+(defun day5-part2-v2 (filepath)
+  (let ((cache (read-all-input-together filepath))
+		(alphabet (mapcar #'list
+						  (concatenate 'list "abcdefghigklmnopqrstuvwxyz")
+						  (concatenate 'list "ABCDEFGHIJKLMNOPQRSTUVWXYZ"))))
+	(loop
+	   for i from 0 to 25
+	   for ab = (nth i alphabet)
+	   collect (let ((inner-cache (remove-from-list cache ab)))
+				 (length (stack-handle inner-cache))))))
 
 (defun case-match (a b)
   (cond ((eql (upper-case-p a) (upper-case-p b))
