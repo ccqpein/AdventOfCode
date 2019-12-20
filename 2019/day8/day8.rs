18e2a2c24f5debddd4b206e1d1b2711169ee887c
ccQpein <ccqpein@protonmail.com>
2019-12-20 07:55:16 -0500

    finish day8

--- 2019/day8/day8.rs
+++ 2019/day8/day8.rs
@@ -1,21 +1,38 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-fn range_bits(mut data: Vec<u8>, wide: usize, tall: usize) -> Vec<Vec<char>> {
-    let mut result: Vec<Vec<char>> = vec![];
+fn day8_part2(filepath: String) {
+    let mut file = File::open(filepath).unwrap();
+    let mut buffer = [0; 25 * 6];
+    let mut buffers: Vec<[u8; 25 * 6]> = vec![];
 
-    while data.len() != 0 {
-        for _ in 0..tall {
-            let mut temp = data
-                .drain(..wide)
-                .map(|c| char::from(c))
-                .collect::<Vec<char>>();
-            temp.push('\n');
-            result.push(temp);
+    loop {
+        match file.read_exact(&mut buffer) {
+            Ok(_) => buffers.push(buffer),
+            Err(_) => break,
         }
     }
 
-    result
+    //re-use buffer for generate image
+    let layer_num = buffers.len();
+
+    for i in 0..25 * 6 {
+        for l in 0..layer_num {
+            if buffers[l][i] != '2' as u8 {
+                if buffers[l][i] == '1' as u8 {
+                    buffer[i] = '@' as u8;
+                } else {
+                    buffer[i] = '.' as u8;
+                }
+                break;
+            }
+        }
+    }
+
+    let chunks = buffer.chunks(25);
+    for i in chunks {
+        println!("{}", String::from_utf8(i.to_vec()).unwrap());
+    }
 }
 
 fn day8(filepath: String) -> usize {
@@ -47,5 +64,6 @@ fn day8(filepath: String) -> usize {
 
 fn main() {
     //dbg!(range_bits("123456789012".as_bytes().to_vec(), 3, 2));
-    dbg!(day8("./day8.input".to_string()));
+    //dbg!(day8("./day8.input".to_string()));
+    dbg!(day8_part2("./day8.input".to_string()));
 }
