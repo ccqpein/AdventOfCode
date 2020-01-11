cd27e6c60d3ae1220f3c27126a9cb0bc8f1f79c2
ccQpein <ccqpein@protonmail.com>
2020-01-11 14:15:08 -0500

    game is on, I think I can stop 2019 AOC now

--- 2019/day25/src/main.rs
+++ 2019/day25/src/main.rs
@@ -1,6 +1,7 @@
 use day11::*;
+use std::str;
 
-fn parse_command(s: String) -> Vec<i64> {
+fn parse_command(s: &String) -> Vec<i64> {
     let mut r = s.chars().map(|x| x as i64).collect::<Vec<i64>>();
     r
 }
@@ -22,9 +23,35 @@ fn main() {
 
     let mut ic = Intcode::new(&intcode);
 
-    let mut buffer = String::new();
     loop {
+        let mut buffer = String::new();
         stdin().read_line(&mut buffer).unwrap();
-        println!("{}", buffer);
+
+        ic.input = parse_command(&buffer);
+        ic.run(None);
+        let output = String::from_utf8(
+            ic.output
+                .clone()
+                .iter()
+                .map(|x| *x as u8)
+                .collect::<Vec<u8>>(),
+        )
+        .unwrap();
+
+        if ic.status == "Terminal" {
+            for l in output.lines() {
+                if l != "" {
+                    println!("{:?}", l)
+                };
+            }
+            break;
+        }
+
+        for l in output.lines() {
+            if l != "" {
+                println!("{:?}", l)
+            };
+        }
+        ic.output = vec![];
     }
 }
