fb9542cb4f5a62dd5a89d0538c5633ff45dc529b
ccQpein <ccqpein@protonmail.com>
2021-11-11 12:17:52 -0500

    a hammer

--- 2020/tools/src/lib.rs
+++ 2020/tools/src/lib.rs
@@ -6,6 +6,7 @@ use std::path::Path;
 mod maps;
 mod stack_machine;
 
+/// read file line by line
 pub fn read_file_by_line(filepath: impl AsRef<Path>) -> Vec<String> {
     let file = File::open(filepath).unwrap();
     BufReader::new(file)
@@ -16,7 +17,7 @@ pub fn read_file_by_line(filepath: impl AsRef<Path>) -> Vec<String> {
 }
 
 /// return same length vec of a, each ind element is a[ind..]
-pub fn vec_of_sum_rest<T>(a: &Vec<T>, mut f: impl FnMut(T, T) -> T) -> Vec<T>
+pub fn vec_of_sum_rest<T>(a: &[T], mut f: impl FnMut(T, T) -> T) -> Vec<T>
 where
     T: Default + Copy,
 {
@@ -27,4 +28,22 @@ where
     });
     buffer.reverse();
     buffer
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_vec_of_sum_rest() {
+        let testcase0 = vec![1, 2, 3, 4, 5];
+        assert_eq!(
+            vec_of_sum_rest(&testcase0, |a, b| { a + b }),
+            vec![15, 14, 12, 9, 5]
+        );
+
+        assert_eq!(
+            vec_of_sum_rest(&[1, 1, 2, 3], |a, b| { a * b }),
+            vec![0, 0, 0, 0]
+        );
+    }
 }
--- aoc2021/README.md
+++ aoc2021/README.md
@@ -3,3 +3,5 @@
 ## Why this name? ##
 
 cargo doesn't allow package name start with digit.
+
+`cargo run --bin day3`
