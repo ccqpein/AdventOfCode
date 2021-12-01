f1befa43e888b8b073cd174ebf4314a796feb203
ccQpein <ccqpein@protonmail.com>
2021-12-01 00:18:41 -0500

    a bit change

--- aoc2021/src/bin/day1/main.rs
+++ aoc2021/src/bin/day1/main.rs
@@ -1,22 +1,25 @@
 use tools::*;
 
-fn part1(input: Vec<String>) {
+fn part1(input: &Vec<String>) {
     let mut count = 0;
-    let mut last = input[0].parse::<i32>().unwrap();
-    for line in &input[1..] {
-        let this = line.parse::<i32>().unwrap();
-        if this > last {
-            count += 1
+    let input = input
+        .iter()
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    input.iter().fold(i32::MAX, |last, this| {
+        if *this > last {
+            count += 1;
         }
-        last = this;
-    }
+        *this
+    });
 
     println!("{}", count);
 }
 
-fn part2(input: Vec<String>) {
+fn part2(input: &Vec<String>) {
     let mut result = 0;
-    let mut input = input
+    let input = input
         .iter()
         .map(|s| s.parse::<i32>().unwrap())
         .collect::<Vec<i32>>();
@@ -39,6 +42,6 @@ fn part2(input: Vec<String>) {
 
 fn main() {
     let input = read_file_by_line("./src/bin/day1/day1.input");
-    part1(input);
-    part2(input);
+    part1(&input);
+    part2(&input);
 }
