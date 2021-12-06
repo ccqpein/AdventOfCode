b25da9380a615d3f9180ce7cf086372aa60372d6
ccQpein <ccqpein@protonmail.com>
2021-12-06 01:21:50 -0500

    clean code

--- aoc2021/src/bin/day6/main.rs
+++ aoc2021/src/bin/day6/main.rs
@@ -18,42 +18,23 @@ fn part1(input: &Vec<String>, day: usize) -> i32 {
     content.iter().count() as i32
 }
 
+/// part2 actuall just faster version of part1
 fn part2(input: &Vec<String>, big_day: usize) -> usize {
     let mut content = input[0]
         .split(",")
         .map(|i| i.parse::<i32>().unwrap())
         .collect::<Vec<_>>();
-    content.sort();
 
-    let mut record = [0; 9];
-    record[0] = content.iter().filter(|f| **f == 0).count();
-    record[1] = content.iter().filter(|f| **f == 1).count();
-    record[2] = content.iter().filter(|f| **f == 2).count();
-    record[3] = content.iter().filter(|f| **f == 3).count();
-    record[4] = content.iter().filter(|f| **f == 4).count();
-    record[5] = content.iter().filter(|f| **f == 5).count();
-    record[6] = content.iter().filter(|f| **f == 6).count();
-    record[7] = content.iter().filter(|f| **f == 7).count();
-    record[8] = content.iter().filter(|f| **f == 8).count();
+    let mut record = (0..=8)
+        .into_iter()
+        .map(|i| content.iter().filter(|f| **f == i).count())
+        .collect::<Vec<_>>();
 
     for d in 1..=big_day {
         let zero = record[0];
-        record[0] = record[1];
-        record[1] = record[2];
-        record[2] = record[3];
-        record[3] = record[4];
-        record[4] = record[5];
-        record[5] = record[6];
-        record[6] = record[7] + zero;
-        record[7] = record[8];
+        (0..8).into_iter().for_each(|i| record[i] = record[i + 1]);
+        record[6] += zero;
         record[8] = zero;
-        // content = content
-        //     .iter()
-        //     .map(|f| if *f != 0 { f - 1 } else { 6 })
-        //     .collect();
-        // content.append(&mut vec![8; num_zero]);
-        //println!("day {}: {:?}", d, record);
-        //let this_day_count = content.iter().count() as i32;
     }
 
     (0..=8).into_iter().map(|i| record[i]).sum()
@@ -62,7 +43,7 @@ fn part2(input: &Vec<String>, big_day: usize) -> usize {
 fn main() {
     let input = read_file_by_line("./src/bin/day6/day6.input");
     //let input = read_file_by_line("./src/bin/day6/day6_demo.input");
-    //println!("{}", part1(&input, 18));
+    println!("{}", part1(&input, 80));
     //println!("{}", part1(&input, 256));
     println!("{}", part2(&input, 256));
 }
