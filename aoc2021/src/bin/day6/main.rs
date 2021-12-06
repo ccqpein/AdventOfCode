617deade26d0ba342218d98011617ea3adf6df7e
ccQpein <ccqpein@protonmail.com>
2021-12-06 01:15:05 -0500

    part 2

--- aoc2021/src/bin/day6/main.rs
+++ aoc2021/src/bin/day6/main.rs
@@ -23,36 +23,40 @@ fn part2(input: &Vec<String>, big_day: usize) -> usize {
         .split(",")
         .map(|i| i.parse::<i32>().unwrap())
         .collect::<Vec<_>>();
+    content.sort();
 
-    let mut record: Vec<i32> = vec![];
+    let mut record = [0; 9];
+    record[0] = content.iter().filter(|f| **f == 0).count();
+    record[1] = content.iter().filter(|f| **f == 1).count();
+    record[2] = content.iter().filter(|f| **f == 2).count();
+    record[3] = content.iter().filter(|f| **f == 3).count();
+    record[4] = content.iter().filter(|f| **f == 4).count();
+    record[5] = content.iter().filter(|f| **f == 5).count();
+    record[6] = content.iter().filter(|f| **f == 6).count();
+    record[7] = content.iter().filter(|f| **f == 7).count();
+    record[8] = content.iter().filter(|f| **f == 8).count();
+
     for d in 1..=big_day {
-        let num_zero = content.iter().filter(|f| **f == 0).count();
-        content = content
-            .iter()
-            .map(|f| if *f != 0 { f - 1 } else { 6 })
-            .collect();
-        content.append(&mut vec![8; num_zero]);
-
-        let this_day_count = content.iter().count() as i32;
-        // if let Some(a) = record
+        let zero = record[0];
+        record[0] = record[1];
+        record[1] = record[2];
+        record[2] = record[3];
+        record[3] = record[4];
+        record[4] = record[5];
+        record[5] = record[6];
+        record[6] = record[7] + zero;
+        record[7] = record[8];
+        record[8] = zero;
+        // content = content
         //     .iter()
-        //     .enumerate()
-        //     .filter(|&(d, c)| *c == this_day_count)
-        //     .next()
-        // {
-        //     println!("day {} fish number is {} as same as day {}", a.0, a.1, d);
-        // }
-
-        record.push(this_day_count);
+        //     .map(|f| if *f != 0 { f - 1 } else { 6 })
+        //     .collect();
+        // content.append(&mut vec![8; num_zero]);
+        //println!("day {}: {:?}", d, record);
+        //let this_day_count = content.iter().count() as i32;
     }
 
-    println!("{:?}", record);
-    let aa = (1..record.len())
-        .into_iter()
-        .map(|ind| record[ind] - record[ind - 1])
-        .collect::<Vec<i32>>();
-    println!("{:?}", aa);
-    0
+    (0..=8).into_iter().map(|i| record[i]).sum()
 }
 
 fn main() {
@@ -60,5 +64,5 @@ fn main() {
     //let input = read_file_by_line("./src/bin/day6/day6_demo.input");
     //println!("{}", part1(&input, 18));
     //println!("{}", part1(&input, 256));
-    println!("{}", part2(&input, 128));
+    println!("{}", part2(&input, 256));
 }
