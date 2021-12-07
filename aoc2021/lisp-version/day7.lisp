cb25b21c7300899b266833356c1c5a2d86c8e686
ccQpein <ccqpein@protonmail.com>
2021-12-07 10:35:34 -0500

    faster faster

--- aoc2021/lisp-version/day7.lisp
+++ aoc2021/lisp-version/day7.lisp
@@ -22,6 +22,31 @@
                        #'<)))
     (loop
       for d from 0 to (car (last content))
-      minimize (apply #'+ (mapcar (lambda (e) (step-consume (abs (- e d)))) content)) into result
-      finally (return result)
+      minimize (apply #'+ (mapcar (lambda (e) (step-consume (abs (- e d)))) content))
       )))
+
+;;; faster implement below
+(defun part1-v2 (path)
+  (let* ((content (sort (mapcar #'parse-integer
+                                (str:split #\, (car (read-file-by-line path))))
+                        #'<))
+         (median-v (floor (/ (+ (nth (floor (/ (1- (length content)) 2)) content)
+                                (nth (floor (/ (length content) 2)) content))
+                             2))))
+    (loop
+      for d in content
+      sum (abs (- d median-v))
+      )))
+
+(defun part2-v2 (path)
+  (let* ((content (sort (mapcar #'parse-integer
+                                (str:split #\, (car (read-file-by-line path))))
+                        #'<))
+         (mean-v (floor (/ (apply #'+ content)
+                           (length content)))))
+    (loop
+      for m from (1- mean-v) to (1+ mean-v)
+      minimize (apply #'+
+                      (mapcar (lambda (v) (let ((diff (abs (- m v))))
+                                            (floor (/ (* diff (1+ diff)) 2))))
+                              content)))))
