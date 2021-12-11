c5007e366563c97ecbde89c46d6a1b0121239e4f
ccQpein <ccqpein@protonmail.com>
2021-12-11 00:53:34 -0500

    day11

---
+++ aoc2021/src/bin/day11/main.rs
@@ -0,0 +1,90 @@
+use tools::*;
+
+fn add1(map: &mut Vec<Vec<usize>>) {
+    for i in 0..map.len() {
+        for j in 0..map[0].len() {
+            map[i][j] += 1
+        }
+    }
+}
+
+fn adjust_energy(map: &mut Vec<Vec<usize>>) {
+    let mut flag = true;
+    while flag {
+        flag = false;
+        for i in 0..map.len() {
+            for j in 0..map[0].len() {
+                if map[i][j] >= 10 {
+                    flag = true;
+                    map[i][j] = 0;
+                    let (ii, jj) = (i as i32, j as i32);
+                    for (x, y) in [
+                        (ii - 1, jj - 1),
+                        (ii - 1, jj),
+                        (ii - 1, jj + 1),
+                        (ii, jj - 1),
+                        (ii, jj + 1),
+                        (ii + 1, jj - 1),
+                        (ii + 1, jj),
+                        (ii + 1, jj + 1),
+                    ] {
+                        if x < 0 || y < 0 || x == map.len() as i32 || y == map[0].len() as i32 {
+                            continue;
+                        }
+
+                        if map[x as usize][y as usize] != 0 {
+                            map[x as usize][y as usize] += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn print_input(input: &Vec<Vec<usize>>) {
+    for l in input {
+        println!("{:?}", l)
+    }
+}
+
+fn get_all_flash(input: &Vec<Vec<usize>>) -> usize {
+    input
+        .iter()
+        .map(|l| l.iter().filter(|d| **d == 0).count())
+        .sum()
+}
+
+fn part1(input: &Vec<String>, step: usize) -> usize {
+    let mut input: Vec<Vec<usize>> = input
+        .iter()
+        .map(|l| {
+            l.chars()
+                .map(|c| c.to_string().parse::<usize>().unwrap())
+                .collect()
+        })
+        .collect();
+
+    let how_many = input.len() * input[0].len();
+    let mut result = 0;
+    for s in 0..step {
+        add1(&mut input);
+        adjust_energy(&mut input);
+        let all = get_all_flash(&input);
+        if all == 100 {
+            println!("at step: {}", s + 1);
+            break;
+        }
+        result += all
+    }
+
+    result
+}
+
+fn main() {
+    let input = read_file_by_line("./src/bin/day11/day11.input");
+    //let input = read_file_by_line("./src/bin/day11/day11_demo.input");
+    println!("{}", part1(&input, 100));
+    println!("{}", part1(&input, usize::MAX - 1));
+    //println!("{}", part2(&input));
+}
