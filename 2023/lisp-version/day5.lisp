16efc9b65122e750396ca6881a0a0551db06c658
ccQpein <ccqpein@protonmail.com>
2023-12-05 13:46:56 -0500

    part2

--- 2023/lisp-version/day5.lisp
+++ 2023/lisp-version/day5.lisp
@@ -6,7 +6,6 @@
 (defun parse-input (input)
   (let (inner-input
         seeds)
-
     (setf inner-input
           (loop
             with result = nil
@@ -19,48 +18,75 @@
               do (push line cache)
             finally (progn (push (reverse cache) result)
                            (return (reverse result)))))
-    ;;(format t "~a~%" inner-input)
-    (setf seeds (mapcar #'parse-integer
-                        (cl-ppcre:all-matches-as-strings "\\d+" (caar inner-input))))
+    (setf seeds
+          (mapcar #'parse-integer
+                  (cl-ppcre:all-matches-as-strings "\\d+" (caar inner-input))))
     
     (append (list seeds)
             (loop for x in (cdr inner-input)
-                  collect (loop for values in (cdr x)
-                           for (ta s len) = (mapcar #'parse-integer
-                                                    (cl-ppcre:all-matches-as-strings "\\d+" values))
-                           collect (list ta s len)
-                           )))))
+                  collect (sort ;; sort for part2
+                           (loop for values in (cdr x)
+                                 for (ta s len)
+                                   = (mapcar #'parse-integer
+                                             (cl-ppcre:all-matches-as-strings "\\d+" values))
+                                 collect (list ta s len)
+                                 )
+                           #'< :key #'cadr)))))
 
 (defun day5 (input)
   (let* ((input (parse-input input))
          (seeds (car input))
          )
     (loop for seed in seeds
-          minimize (loop for map in (cdr input)
-                         do  (setf seed
-                                   (loop for (ta s len) in map
-                                         if (<= s seed (+ s len))
-                                           return (+ ta (- seed s))
-                                         finally (return seed)))
-                         finally (return seed)))))
+          minimize (loop
+                     for map in (cdr input)
+                     do (setf seed
+                              (loop for (ta s len) in map
+                                    if (<= s seed (+ s len))
+                                      return (+ ta (- seed s))
+                                    finally (return seed)))
+                     finally (return seed)))))
+
+;; steal from friend
+(defun get-range (range map)
+  (let* ((start (car range))
+         (end (cadr range))
+         (range '()))
+    (loop
+      for (tar s-start len) in map
+      do (let ((s-end (+ s-start len)) ;; not including
+               )
+           (when (< start s-end)
+             (if (< start s-start)
+                 (push (list start (min s-start end)) range))
+           
+             (setf start (max start s-start))
+             (if (> (min s-end end) start)
+                 (push (list (+ start (- s-start) tar)
+                             (+ (min s-end end) (- s-start) tar))
+                       range))
+           
+             (setf start s-end)
+             (if (>= start end)
+                 (return)))
+           ))    
+    (if (< start end) (push (list start end) range))
+    (reverse range)))
 
 (defun day5-2 (input)
   (let* ((input (parse-input input))
-         seeds 
-         )
-    (setf seeds
-          (remove-duplicates
-           (loop for (start len) on (car input) by #'cddr
-                 append (loop for x from 0 below len collect (+ x start)))))
-    seeds
+         (seeds (loop for (start range) on (car input) by #'cddr
+                      collect (list start (+ start range))))
+         ranges-result)
     
-    ;; (loop for seed in seeds
-    ;;       minimize (loop for map in (cdr input)
-    ;;                      do  (setf seed
-    ;;                                (loop for (ta s len) in map
-    ;;                                      if (<= s seed (+ s len))
-    ;;                                        return (+ ta (- seed s))
-    ;;                                      finally (return seed)))
-    ;;                      finally (return seed)))
-  ))
-
+    (setf ranges-result
+          (loop for seed in seeds
+                append (loop
+                         with cache = (list seed)
+                         for map in (cdr input)
+                         do (setf cache
+                                  (loop for seed in cache
+                                        append (get-range seed map) into result
+                                        finally (return result)))
+                         finally (return cache))))
+    (caar (sort ranges-result #'< :key #'car))))
