9f19d0efc4273866102fa71512e7824120ed6a0a
ccQpein <ccqpein@protonmail.com>
2024-03-05 01:10:33 -0500

    day6

--- 2015/rust-version/src/bin/day6/main.rs
+++ 2015/rust-version/src/bin/day6/main.rs
@@ -3,23 +3,29 @@ use tools::*;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Square {
+    on: bool,
     // left upper corner
     lu: (i64, i64),
     // right down corner
     rd: (i64, i64),
 }
 
-pub fn joint_square(a: &Square, b: &Square) -> Square {
+pub fn joint_square(a: &Square, b: &Square) -> Option<Square> {
     let x1 = a.lu.0.max(b.lu.0);
     let y1 = a.lu.1.max(b.lu.1);
+
+    if (x1 >= a.rd.0 && y1 >= a.rd.1) || (x1 >= b.rd.0 && y1 >= b.rd.1) {
+        return None;
+    }
 
     let x2 = a.rd.0.min(b.rd.0);
     let y2 = a.rd.1.min(b.rd.1);
 
-    Square {
+    Some(Square {
+        on: false,
         lu: (x1, y1),
         rd: (x2, y2),
-    }
+    })
 }
 
 fn parse(s: &str) -> (&str, Square) {
@@ -28,6 +34,7 @@ fn parse(s: &str) -> (&str, Square) {
     (
         comm,
         Square {
+            on: false,
             lu: (x1.parse().unwrap(), y1.parse().unwrap()),
             rd: (x2.parse().unwrap(), y2.parse().unwrap()),
         },
@@ -35,12 +42,52 @@ fn parse(s: &str) -> (&str, Square) {
 }
 
 fn day6(input: &[String]) -> usize {
-    input.iter().map(|line| parse(line));
-    0
+    let re = Regex::new(r"^(.+) (\d+),(\d+) through (\d+),(\d+)$").unwrap();
+    let mut whole_map = [[0; 1000]; 1000];
+    for line in input {
+        let (_, [comm, x1, y1, x2, y2]) = re.captures(line).unwrap().extract();
+        let (x1, y1, x2, y2) = (
+            x1.parse::<usize>().unwrap(),
+            y1.parse::<usize>().unwrap(),
+            x2.parse::<usize>().unwrap(),
+            y2.parse::<usize>().unwrap(),
+        );
+        match comm {
+            "turn on" => {
+                for y in y1.min(y2)..=y1.max(y2) {
+                    for x in x1.min(x2)..=x1.max(x2) {
+                        whole_map[y][x] = 1;
+                    }
+                }
+            }
+            "turn off" => {
+                for y in y1.min(y2)..=y1.max(y2) {
+                    for x in x1.min(x2)..=x1.max(x2) {
+                        whole_map[y][x] = 0;
+                    }
+                }
+            }
+            "toggle" => {
+                for y in y1.min(y2)..=y1.max(y2) {
+                    for x in x1.min(x2)..=x1.max(x2) {
+                        whole_map[y][x] ^= 1;
+                    }
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    whole_map
+        .iter()
+        .map(|l| l.iter().sum::<usize>())
+        .sum::<usize>()
 }
 
 fn main() {
     let input = read_file_by_line("../inputs/day6.input");
+    //let input = vec!["turn on 0,0 through 999,999".to_string()];
+    println!("{:?}", day6(&input));
 }
 
 #[cfg(test)]
@@ -51,18 +98,21 @@ mod tests {
     fn test_joint_square() {
         let a = joint_square(
             &Square {
+                on: false,
                 lu: (0, 0),
                 rd: (10, 10),
             },
             &Square {
+                on: false,
                 lu: (0, 0),
                 rd: (10, 10),
             },
         );
 
         assert_eq!(
-            a,
+            a.unwrap(),
             Square {
+                on: false,
                 lu: (0, 0),
                 rd: (10, 10),
             },
@@ -70,18 +120,21 @@ mod tests {
 
         let a = joint_square(
             &Square {
+                on: false,
                 lu: (0, 0),
                 rd: (10, 10),
             },
             &Square {
+                on: false,
                 lu: (5, 5),
                 rd: (11, 11),
             },
         );
 
         assert_eq!(
-            a,
+            a.unwrap(),
             Square {
+                on: false,
                 lu: (5, 5),
                 rd: (10, 10),
             },
@@ -89,18 +142,21 @@ mod tests {
 
         let a = joint_square(
             &Square {
+                on: false,
                 lu: (0, 0),
                 rd: (10, 10),
             },
             &Square {
+                on: false,
                 lu: (5, 5),
                 rd: (9, 9),
             },
         );
 
         assert_eq!(
-            a,
+            a.unwrap(),
             Square {
+                on: false,
                 lu: (5, 5),
                 rd: (9, 9),
             },
@@ -108,22 +164,40 @@ mod tests {
 
         let a = joint_square(
             &Square {
+                on: false,
                 lu: (0, 0),
                 rd: (10, 10),
             },
             &Square {
+                on: false,
                 lu: (5, 5),
                 rd: (9, 11),
             },
         );
 
         assert_eq!(
-            a,
+            a.unwrap(),
             Square {
+                on: false,
                 lu: (5, 5),
                 rd: (9, 10),
             },
         );
+
+        let a = joint_square(
+            &Square {
+                on: false,
+                lu: (0, 0),
+                rd: (5, 5),
+            },
+            &Square {
+                on: false,
+                lu: (5, 5),
+                rd: (9, 11),
+            },
+        );
+
+        assert_eq!(a, None);
     }
 
     #[test]
@@ -133,6 +207,7 @@ mod tests {
             (
                 "turn off",
                 Square {
+                    on: false,
                     lu: (278, 258),
                     rd: (367, 386)
                 }
