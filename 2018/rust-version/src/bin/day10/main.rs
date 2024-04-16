40e3f63c6d4763243dcdf57c716b38de9d6267af
ccQpein <ccqpein@protonmail.com>
2024-04-15 22:18:59 -0400

    a bit

---
+++ 2018/rust-version/src/bin/day10/main.rs
@@ -0,0 +1,10 @@
+use regex::*;
+use tools::*;
+
+fn day10(input: &[String]) {
+    let re = Regex::new(r#"position=<([- ]?\d+), ([- ]?\d+)> velocity=<([- ]?\d+), ([- ]?\d+)>"#);
+}
+
+fn main() {
+    let input = read_file_by_line("../inputs/day10.input");
+}
