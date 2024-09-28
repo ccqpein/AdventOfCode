533ad04d894e3838127490179fd32c5bd1d2a0e8
ccQpein <ccqpein@protonmail.com>
2024-09-27 21:02:31 -0400

    part1 day12

---
+++ 2016/lisp-version/day12.lisp
@@ -0,0 +1,57 @@
+(load "../../tools/tools.lisp")
+
+(defparameter *input* (read-file-by-line "../inputs/day12.input"))
+(defparameter *input-demo*
+  (str:lines "cpy 41 a
+inc a
+inc a
+dec a
+jnz a 2
+dec a"))
+
+(defun handle-lines (lines table)
+  (do* ((ind 0)
+        (line (nth ind lines) (nth ind lines)))
+       ((>= ind (length lines)))
+    (str:match line
+      (("cpy " x " " y)
+       (setf (gethash y table) (handler-case (parse-integer x)
+                                 (error (e)
+                                   (declare (ignore e))
+                                   (gethash-or-insert x table 0))))
+       (incf ind)
+       ;;(format t "copy ~a to ~a~%" x y)
+       )
+      (("inc " x)
+       (setf (gethash x table) (1+ (gethash-or-insert x table 0)))
+       (incf ind)
+       ;;(format t "inc ~a to ~a~%" x (gethash x table))
+       )
+      (("dec " x)
+       (setf (gethash x table) (1- (gethash-or-insert x table 0)))
+       (incf ind)
+       ;;(format t "dec ~a to ~a~%" x (gethash x table))
+       )
+      (("jnz " x " " y)
+       (let (n)
+         (handler-case (parse-integer x)
+           (error (e)
+             (declare (ignore e))
+             (setf n (gethash-or-insert x table 0)))
+           (:no-error (a b) (setf n a)))
+
+         (if (/= n 0)
+             (incf ind (parse-integer y))
+             (incf ind)))))
+    ))
+
+(defun gethash-or-insert (key table init)
+  (if (gethash key table)
+      (gethash key table)
+      (progn (setf (gethash key table) init)
+             (gethash key table))))
+
+(defun part1 (&optional (input *input*))
+  (let ((table (make-hash-table :test 'equal)))
+    (handle-lines input table)
+    (gethash "a" table)))
