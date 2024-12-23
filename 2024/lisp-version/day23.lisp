49b788ce50eecb5eaee69a28d9cc6c608508e3ec
ccQpein <ccqpein@protonmail.com>
2024-12-23 17:31:42 -0500

    fix

--- 2024/lisp-version/day23.lisp
+++ 2024/lisp-version/day23.lisp
@@ -57,3 +57,38 @@
       (format nil "~{~a~^,~}"
               (sort (second (first (sort g-with-len #'> :key #'first)))
                     #'string<)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; should be the "real" correct solution below
+
+(defun all-connect-2 (g rest cache)
+  (if (gethash rest cache) (return-from all-connect-2 (gethash rest cache)))
+  (if (= 1 (length rest)) (return-from all-connect-2 (list rest)))
+  (let ((result (loop for this in rest
+                      for all-ns = (get-all-nodes-of-id-without-weight g this)
+                      for next-round = (sort (loop for n in rest
+                                                   when (member n all-ns :test 'equal)
+                                                     collect n)
+                                             #'string<)
+                      append (loop for g in (all-connect-2 g next-round cache)
+                                   collect (cons this g)))))
+    (setf (gethash rest cache)
+          (remove-duplicates-g result))
+    (gethash rest cache)))
+
+(defun remove-duplicates-g (gs)
+  (remove-duplicates (mapcar (lambda (g) (sort (copy-seq g) #'string<)) gs) :test 'equal))
+
+(defun day23-21 (input)
+  (let ((g (parse-input input)))
+    (let ((all-nodes (get-all-nodes g))
+          (table (make-hash-table :test 'equal)))
+      (let ((re (remove-duplicates-g
+                 (loop for nn in all-nodes
+                       for children = (sort (mapcar #'car (cdr nn)) #'string<)
+                       append
+                       (mapcar (lambda (c) (cons (first nn) c)) (all-connect-2 g children table))))))
+        (let ((g-with-len (mapcar (lambda (gg) (list (length gg) gg)) re)))
+          (format nil "~{~a~^,~}"
+                  (sort (second (first (sort g-with-len #'> :key #'first)))
+                        #'string<)))))))
