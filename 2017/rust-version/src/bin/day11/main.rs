24e316554cabc9ca9c9be3c8ad869c84d65c6911
ccQpein <ccqpein@protonmail.com>
2025-03-03 22:41:34 -0500

    let me try day11

---
+++ 2017/rust-version/src/bin/day11/main.rs
@@ -0,0 +1,10 @@
+use itertools::*;
+use tools::*;
+
+fn parse_input(input: &[String]) -> Vec<&str> {
+    input[0].split(",").collect()
+}
+
+fn main() {
+    let input = read_file_by_line("../inputs/day11.input");
+}
