20ba53ff72dfd24b3b6c95a7b32647bc4e3f1df9
ccQpein <ccqpein@protonmail.com>
2025-03-07 22:52:02 -0500

    part1

---
+++ 2017/rust-version/src/bin/day13/main.rs
@@ -0,0 +1,45 @@
+use std::{array::IntoIter, collections::HashMap};
+
+use regex::Regex;
+use tools::*;
+
+fn day13(inputs: &[String]) -> i32 {
+    let table = parse_input(inputs).collect::<HashMap<_, _>>();
+    let mut re = 0;
+    for time in 0..=*table.keys().max().unwrap_or(&0) {
+        if let Some(len) = table.get(&time) {
+            if find_ind(time, *len) == 0 {
+                re += time * len;
+            }
+        }
+    }
+    re
+}
+
+fn find_ind(time: i32, len: i32) -> i32 {
+    let a = time % (2 * (len - 1));
+    if a < len { a } else { (2 * (len - 1)) - a }
+}
+
+fn parse_input(inputs: &[String]) -> impl Iterator<Item = (i32, i32)> {
+    let re = Regex::new(r"^(\d+): (\d+)$").unwrap();
+    inputs.into_iter().map(move |l| {
+        let Some(c) = re.captures(l) else { panic!() };
+
+        (c[1].parse::<i32>().unwrap(), c[2].parse::<i32>().unwrap())
+    })
+}
+
+fn main() {
+    let input = read_file_by_line("../inputs/day13.input");
+    //dbg!(parse_input(&vec!["40: 12".to_string()]));
+
+    dbg!(find_ind(5, 4));
+    dbg!(find_ind(4, 4));
+    dbg!(find_ind(6, 4));
+    dbg!(find_ind(7, 4));
+    dbg!(find_ind(0, 3));
+    dbg!(find_ind(1, 3));
+
+    dbg!(day13(&input));
+}
