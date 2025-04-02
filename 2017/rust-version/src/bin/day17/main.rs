cdc5f7e476e3246159aaa9e1f676b6156e1efb69
ccQpein <ccqpein@protonmail.com>
2025-04-01 23:42:42 -0400

    start day17

---
+++ 2017/rust-version/src/bin/day17/main.rs
@@ -0,0 +1,10 @@
+fn count_next_ind(current: usize, len: usize, num: usize) -> usize {
+    //current + num
+}
+
+fn day17(times: usize) {
+    let mut stack = vec![0]; // yep, it is slow
+    for i in 1..=times {}
+}
+
+fn main() {}
