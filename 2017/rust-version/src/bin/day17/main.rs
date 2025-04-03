0054161bce3178eacd4faed127e0400ce807765e
ccQpein <ccqpein@protonmail.com>
2025-04-03 14:15:55 -0400

    part2

--- 2017/rust-version/src/bin/day17/main.rs
+++ 2017/rust-version/src/bin/day17/main.rs
@@ -25,11 +25,29 @@ fn day17(times: usize, num: usize) {
     );
 }
 
+fn day17_2(times: usize, num: usize) {
+    let mut stack = vec![0]; // yep, it is slow
+    let mut current_ind = 0;
+    let mut len = stack.len();
+    for i in 1..=times {
+        //println!("{:?}", stack);
+        let next_ind = count_next_ind(current_ind, len, num);
+        if next_ind == 1 {
+            println!("time {}", i);
+        }
+
+        len += 1;
+        current_ind = next_ind
+    }
+    println!("ind: {}", current_ind,);
+}
+
 fn main() {
     dbg!(count_next_ind(0, 1, 3));
     dbg!(count_next_ind(1, 2, 3));
     dbg!(count_next_ind(1, 3, 3));
 
     //day17(2017, 3);
-    day17(2017, 367);
+    //day17(2017, 367);
+    day17_2(50000000, 367);
 }
