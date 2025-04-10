80fa0f2d69c3b10571186288ffd98d1e8f649a46
ccQpein <ccqpein@protonmail.com>
2025-04-09 23:31:29 -0400

    day18 part2 yeahhhhhh

--- 2017/rust-version/src/bin/day18/main.rs
+++ 2017/rust-version/src/bin/day18/main.rs
@@ -1,8 +1,12 @@
-use core::panic;
-use std::{collections::HashMap, mem};
+use std::{collections::HashMap, time::Duration};
 
 use regex::Regex;
-use tools::*;
+use tokio::{
+    select,
+    sync::mpsc::{self, *},
+    time,
+};
+use tools::read_file_by_line;
 
 fn parse_inputs(inputs: &[String]) -> Vec<(String, String, Option<String>)> {
     let re = Regex::new(r"^(\w+)\s+(\d+|[a-z])(\s+(-?\d+|[a-z]))?$").unwrap();
@@ -52,11 +56,6 @@ fn day18(inputs: &[String]) {
                     Err(_) => *memory.entry(&c.as_ref().unwrap()).or_insert(0),
                 };
 
-                // match memory.get_mut(b) {
-                //     Some(bb) => *bb += digt,
-                //     None => (),
-                // }
-
                 *memory.entry(b).or_insert(0) += digt
             }
             ("mul", _, _) => {
@@ -65,11 +64,6 @@ fn day18(inputs: &[String]) {
                     Err(_) => *memory.entry(&c.as_ref().unwrap()).or_insert(0),
                 };
 
-                // match memory.get_mut(b) {
-                //     Some(bb) => *bb *= digt,
-                //     None => (),
-                // }
-
                 *memory.entry(b).or_insert(0) *= digt
             }
             ("mod", _, _) => {
@@ -77,11 +71,6 @@ fn day18(inputs: &[String]) {
                     Ok(d) => d,
                     Err(_) => *memory.entry(&c.as_ref().unwrap()).or_insert(0),
                 };
-
-                // match memory.get_mut(b) {
-                //     Some(bb) => *bb %= digt,
-                //     None => (),
-                // }
 
                 *memory.entry(b).or_insert(0) %= digt;
             }
@@ -92,22 +81,11 @@ fn day18(inputs: &[String]) {
                 }
             }
             ("jgz", _, _) => {
-                // match memory.get_mut(b) {
-                //     Some(bb) => {
-                //         if *bb > 0 {
-                //             ind += c.as_ref().unwrap().parse::<i64>().unwrap();
-                //             continue;
-                //         }
-                //     }
-                //     None => (),
-                // }
-
                 let digt = match b.parse::<i64>() {
                     Ok(d) => d,
                     Err(_) => *memory.entry(b).or_insert(0),
                 };
 
-                //let en = memory.entry(b).or_insert(0);
                 if digt > 0 {
                     ind += c.as_ref().unwrap().parse::<i64>().unwrap();
                     continue;
@@ -122,10 +100,136 @@ fn day18(inputs: &[String]) {
     println!("last: {last}")
 }
 
-fn main() {
+///////////////////////////////////////////////
+//////////////////////////////////////////////
+
+async fn one_program(
+    input: Vec<(String, String, Option<String>)>,
+    id: i64,
+    snd: Sender<i64>,
+    mut rev: Receiver<i64>,
+) {
+    let mut ind = 0;
+    let mut memory = HashMap::new();
+    memory.insert("p".to_string(), id);
+
+    loop {
+        let Some((a, b, c)) = input.get(ind as usize) else {
+            break;
+        };
+
+        match (a.as_str(), b.as_str(), c.as_ref().map(|v| v.as_str())) {
+            ("snd", _, None) => {
+                let v = *memory.entry(b.to_string()).or_insert(0);
+                snd.send(v).await;
+            }
+            ("set", _, _) => {
+                let digt = match c.as_ref().unwrap().parse::<i64>() {
+                    Ok(d) => d,
+                    Err(_) => *memory.entry(c.as_ref().unwrap().to_string()).or_insert(0),
+                };
+
+                *memory.entry(b.to_string()).or_insert(0) = digt
+            }
+            ("add", _, _) => {
+                let digt = match c.as_ref().unwrap().parse::<i64>() {
+                    Ok(d) => d,
+                    Err(_) => *memory.entry(c.as_ref().unwrap().to_string()).or_insert(0),
+                };
+
+                *memory.entry(b.to_string()).or_insert(0) += digt
+            }
+            ("mul", _, _) => {
+                let digt = match c.as_ref().unwrap().parse::<i64>() {
+                    Ok(d) => d,
+                    Err(_) => *memory.entry(c.as_ref().unwrap().to_string()).or_insert(0),
+                };
+
+                *memory.entry(b.to_string()).or_insert(0) *= digt
+            }
+            ("mod", _, _) => {
+                let digt = match c.as_ref().unwrap().parse::<i64>() {
+                    Ok(d) => d,
+                    Err(_) => *memory.entry(c.as_ref().unwrap().to_string()).or_insert(0),
+                };
+
+                *memory.entry(b.to_string()).or_insert(0) %= digt;
+            }
+            ("rcv", _, _) => {
+                let v = rev.recv().await.unwrap();
+                memory.insert(b.to_string(), v);
+            }
+            ("jgz", _, _) => {
+                let digt = match b.parse::<i64>() {
+                    Ok(d) => d,
+                    Err(_) => *memory.entry(b.to_string()).or_insert(0),
+                };
+
+                let jmp_step = match c.clone().unwrap().parse::<i64>() {
+                    Ok(d) => d,
+                    Err(_) => *memory.entry(c.clone().unwrap().to_string()).or_insert(0),
+                };
+
+                if digt > 0 {
+                    ind += jmp_step;
+                    continue;
+                }
+            }
+            _ => unreachable!(),
+        }
+
+        ind += 1
+    }
+}
+
+async fn day18_p2(inputs: &[String]) {
+    let (tx0, mut rx0) = mpsc::channel(10000);
+    let (tx01, mut rx01) = mpsc::channel(10000);
+
+    let (tx1, mut rx1) = mpsc::channel(10000);
+    let (tx11, mut rx11) = mpsc::channel(10000);
+
+    let p_inputs = parse_inputs(inputs);
+
+    tokio::spawn(one_program(p_inputs.clone(), 0, tx1, rx01));
+    tokio::spawn(one_program(p_inputs.clone(), 1, tx0, rx11));
+
+    // stop flag design
+    let mut interval = time::interval(Duration::from_secs(1));
+    let mut stop_flag = false;
+
+    let mut send_from_1 = 0;
+    let mut send_from_0 = 0;
+    loop {
+        select! {
+            Some(v0) = rx0.recv() => {
+                send_from_1 +=1;
+                tx01.send(v0).await;
+                stop_flag = false;
+            }
+            Some(v1) = rx1.recv() => {
+                send_from_0 +=1;
+                tx11.send(v1).await;
+                stop_flag = false;
+            }
+            _ = interval.tick() => {
+                if stop_flag {
+                    break
+                }else {
+                    stop_flag = true
+                }
+            }
+        }
+        println!("send from 1: {send_from_1}, send from 0: {send_from_0}")
+    }
+}
+
+#[tokio::main]
+async fn main() {
     let input = read_file_by_line("../inputs/day18.input");
     //let input = read_file_by_line("../inputs/day18_demo.input");
     //parse_inputs(&input);
 
-    day18(&input);
+    //day18(&input);
+    day18_p2(&input).await;
 }
